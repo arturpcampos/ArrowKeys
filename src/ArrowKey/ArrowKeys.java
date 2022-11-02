fc7f0dd865b7ca1dd6049f897b9d5723888718b7
arturpcampos <arturpcampos@hotmail.com>
2022-11-01 23:40:32 -0400

    ArrowKeys final

--- src/ArrowKey/ArrowKeys.java
+++ src/ArrowKey/ArrowKeys.java
@@ -1,6 +1,10 @@
 package ArrowKey;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ArrowKeys {
@@ -13,7 +17,60 @@ public class ArrowKeys {
 		frame.setFocusable(true);
 		
 		JPanel panel = new JPanel();
+		JLabel up = new JLabel();
+		JLabel down = new JLabel();
+		JLabel left = new JLabel();
+		JLabel right = new JLabel();
 		
+		panel.add(up);
+		panel.add(down);
+		panel.add(left);
+		panel.add(right);
+		
+		up.setText("Up: 0");
+		down.setText("Down: 0");
+		left.setText("Left: 0");
+		right.setText("Right: 0");
+		
+		frame.addKeyListener(new KeyListener() {
+			int upCount = 0;
+			int downCount = 0;
+			int leftCount = 0;
+			int rightCount = 0;
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int keyCode = e.getKeyCode();
+				switch (keyCode) {
+				case KeyEvent.VK_4:
+					up.setText("Up: " + Integer.toString(upCount++));
+					break;
+				case KeyEvent.VK_DOWN:
+					down.setText("Down: " + Integer.toString(downCount++));
+					break;
+				case KeyEvent.VK_RIGHT:
+					right.setText("Right: " + Integer.toString(rightCount++));
+					break;
+				case KeyEvent.VK_LEFT:
+					left.setText("Left: " + Integer.toString(leftCount++));
+					break;
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		frame.add(panel);
 	}
 	
 	public static void main(String[] args) {
