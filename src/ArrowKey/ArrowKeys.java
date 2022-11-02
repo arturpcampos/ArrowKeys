745ac3cfe0868672dcb7a601b2aca753b5b629bc
arturpcampos <arturpcampos@hotmail.com>
2022-11-01 23:10:57 -0400

    ArrowKeys

---
+++ src/ArrowKey/ArrowKeys.java
@@ -0,0 +1,25 @@
+package ArrowKey;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ArrowKeys {
+
+	public ArrowKeys() {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setSize(400,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setFocusable(true);
+		
+		JPanel panel = new JPanel();
+		
+	}
+	
+	public static void main(String[] args) {
+		new ArrowKeys();
+		
+	}
+	
+	
+}
